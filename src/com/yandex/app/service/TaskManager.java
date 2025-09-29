1884bffdf36942f5c7765f3515e6a5bc94d97bdf
Aleksandr Kovalenko <kovalenlo1234as@yandex.ru>
2025-09-30 02:54:13 +0300

    Исправил конфликты

--- src/com/yandex/app/service/TaskManager.java
+++ src/com/yandex/app/service/TaskManager.java
@@ -6,30 +6,44 @@ import com.yandex.app.model.Task;
 import java.util.List;
 
 public interface TaskManager {
+
     List<Task> getAllTasks();
+
     List<Epic> getAllEpics();
+
     List<Subtask> getAllSubtasks();
 
     void deleteAllTasks();
+
     void deleteAllEpics();
+
     void deleteAllSubtasks();
 
     Task getTaskById(int id);
+
     Epic getEpicById(int id);
+
     Subtask getSubtaskById(int id);
 
     void addTask(Task task);
+
     void addEpic(Epic epic);
+
     void addSubtask(Subtask subtask);
 
     void updateTask(Task task);
+
     void updateEpic(Epic epic);
+
     void updateSubtask(Subtask subtask);
 
     void deleteTaskById(int id);
+
     void deleteEpicById(int id);
+
     void deleteSubtaskById(int id);
 
     List<Subtask> getSubtasksByEpicId(int epicId);
+
     List<Task> getHistory();
 }
