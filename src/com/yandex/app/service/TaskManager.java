f8554b79c987792c825a1eff6c8671dea363223b
Aleksandr Kovalenko <kovalenlo1234as@yandex.ru>
2025-10-04 03:16:08 +0300

    Исправил конфликты

--- src/com/yandex/app/service/TaskManager.java
+++ src/com/yandex/app/service/TaskManager.java
@@ -7,31 +7,50 @@ import java.util.List;
 import java.util.Set;
 
 public interface TaskManager {
+
     List<Task> getAllTasks();
+
     void deleteAllTasks();
+
     Task getTaskById(int id);
+
     Task createTask(Task task);
+
     void updateTask(Task task);
+
     void deleteTask(int id);
 
     List<Epic> getAllEpics();
+
     void deleteAllEpics();
+
     Epic getEpicById(int id);
+
     Epic createEpic(Epic epic);
+
     void updateEpic(Epic epic);
+
     void deleteEpic(int id);
 
     List<Subtask> getAllSubtasks();
+
     void deleteAllSubtasks();
+
     Subtask getSubtaskById(int id);
+
     Subtask createSubtask(Subtask subtask);
+
     void updateSubtask(Subtask subtask);
+
     void deleteSubtask(int id);
 
     List<Subtask> getSubtasksByEpicId(int epicId);
+
     List<Task> getHistory();
 
     Set<Task> getPrioritizedTasks();
+
     boolean hasTimeOverlap(Task task1, Task task2);
+
     boolean isTimeSlotAvailable(Task task);
 }
